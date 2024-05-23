2185d0cfcc2157f3d3d09ea393339b762387f9e4
prathamesh nale <prathameshn7077@gmail.com>
2024-05-23 12:25:11 +0530

    add new points

---
+++ 9_installtion_nodeJS.js
@@ -0,0 +1,18 @@
+/* 
+
+Installation of node js in local machine - 
+
+1. IF we run our javaScript code on any browser then it is called as JavaScript and 
+2. if we run our javaScript code on our local machine then it is called as nodeJS.
+
+NodeJS - NodeJs is a execution engine of javaScript code in our local machine/editor or server.
+
+npm - npm is a package manager in nodeJS . (which handle all dependency)
+
+Creat react app from cmd - 
+1.creat folder in local machine and open it from cmd
+2.type command 'npx create-react-app nameOfApp'  and then enter, all the neccessory packages will be downloaded
+  from the react server.
+
+
+*/
